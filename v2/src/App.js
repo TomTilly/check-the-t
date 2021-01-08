60c6b20644b78663882d2302576b0f7756801d7f
Tom Tillistrand <tomtillistrand@gmail.com>
2021-01-08 11:16:00 -0500

    Add routes

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
+import ArrivalCalc from './ArrivalCalc';
+import About from './About';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={ArrivalCalc} />
+        <Route exact path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ v2/src/ArrivalCalc.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ArrivalCalc extends Component {
+  render() {
+    return <div className="ArrivalCalc">ArrivalCalc</div>;
+  }
+}
+
+export default ArrivalCalc;
