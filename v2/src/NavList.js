6f84fcd5e5bfff4a37c2dee6c0b7e780b9439828
Tom Tillistrand <tomtillistrand@gmail.com>
2021-01-08 10:44:26 -0500

    Make menu animate from top

--- v2/src/NavList.js
+++ v2/src/NavList.js
@@ -15,57 +15,7 @@ class NavList extends Component {
     this.navList = React.createRef();
   }
 
-  expandMenu() {
-    const { navList } = this;
-    // get the height of the element's inner content, regardless of its actual size
-    const menuHeight = navList.scrollHeight;
-
-    // have the element transition to the height of its inner content
-    navList.style.height = `${menuHeight}px`;
-
-    // when the next css transition finishes (which should be the one we just triggered)
-    navList.addEventListener(
-      'transitionend',
-      function () {
-        // remove "height" from the element's inline styles, so it can return to its initial value
-        navList.style.height = null;
-      },
-      { once: true }
-    );
-  }
-
-  collapseMenu() {
-    const { navList } = this;
-
-    // get the height of the element's inner content, regardless of its actual size
-    const menuHeight = navList.scrollHeight;
-
-    // temporarily disable all css transitions
-    const elementTransition = navList.style.transition;
-    navList.style.transition = '';
-
-    // on the next frame (as soon as the previous style change has taken effect),
-    // explicitly set the element's height to its current pixel height, so we
-    // aren't transitioning out of 'auto'
-    requestAnimationFrame(function () {
-      navList.style.height = `${menuHeight}px`;
-      navList.style.transition = elementTransition;
-
-      // on the next frame (as soon as the previous style change has taken effect),
-      // have the element transition to height: 0
-      requestAnimationFrame(function () {
-        navList.style.height = `${0}px`;
-      });
-    });
-  }
-
   toggleMenu() {
-    const { isMenuActive } = this.state;
-    if (isMenuActive) {
-      this.collapseMenu();
-    } else {
-      this.expandMenu();
-    }
     this.setState((st) => ({ isMenuActive: !st.isMenuActive }));
   }
 
